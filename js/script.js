ba4b414f1e3505c96070379ded63dec4db4cff7c
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-08 15:15:35 +0200

    Es. completo: JS-Vue completamente funzionante

--- js/script.js
+++ js/script.js
@@ -2,11 +2,18 @@ function initVue() {
     new Vue({
         el: '#vue',
         data:{
-            todoList: ['Compra il pane', 'Porta fuori la spazzatura', 'Lezioni Boolean', 'Fai workout corpo libero']
+            todoList: ['Compra il pane', 'Porta fuori la spazzatura', 'Lezioni Boolean', 'Fai workout corpo libero'],
+            newTodo: ''
         },
         methods:{
-            prova: function () {
-                console.log('OOOOOOO');
+            addNewTodo: function () {
+                if(this.newTodo[0] != ' ' && this.newTodo.length > 0){
+                    this.todoList.push(this.newTodo);
+                    this.newTodo = '';
+                }
+            },
+            removeTodo: function (index) {
+                this.todoList.splice(index, 1);
             }
         }
     });
